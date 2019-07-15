a9045449768c7e19137242b6eec60b535ad39e68
Joe Domovitch <jdomovitch@gmail.com>
2019-07-14 21:14:21 -0700

    project 1 assessment complete

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//grab elements
+const h1Total = document.querySelector('h1');
+const input = document.querySelector('input');
+const add = document.querySelector('#add');
+const sub = document.querySelector('#subtract');
+
+//event listeners
+add.addEventListener('click', () => {
+  h1Total.innerText = parseInt(input.value) + parseInt(h1Total.innerText);
+  checkNegative();
+})
+
+sub.addEventListener('click', () => {
+  h1Total.innerText = parseInt(h1Total.innerText) - parseInt(input.value);
+  checkNegative();
+})
+
+//turn h1 text red if number is negative
+const checkNegative = () => {
+  if (parseInt(h1Total.innerText) < 0) h1Total.classList.add('red');
+  else h1Total.classList.remove('red');
+}
